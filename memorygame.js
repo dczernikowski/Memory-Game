6fe7bf2f4411685c8184261551921d74cb5ebc1c
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2019-02-27 12:51:41 -0500

    adding everything and links to images

--- memorygame.js
+++ memorygame.js
@@ -1,3 +1,18 @@
-function restart {
-  
+function restart() {
+
+}
+
+function club() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("club1").src = picture;
+}
+
+function twoCardsFlipped() {
+
 }
