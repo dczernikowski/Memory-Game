ef1b607997ba3f14666cadd6dc1cd1b954f32869
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2019-02-27 13:18:06 -0500

    change the picture to their correct pictures

--- memorygame.js
+++ memorygame.js
@@ -24,6 +24,115 @@ function clubTwo() {
   document.getElementById("club2").src = picture;
 }
 
+function triangleOne() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("triangle1").src = picture;
+}
+
+function triangleTwo() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("triangle2").src = picture;
+}
+
+function diamondOne() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("diamond1").src = picture;
+}
+
+function diamondTwo() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("diamond2").src = picture;
+}
+
+function smileyOne() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("smiley1").src = picture;
+}
+
+function smileyTwo() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("smiley2").src = picture;
+}
+
+function spadeOne() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("spade1").src = picture;
+}
+
+function spadeTwo() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("spade2").src = picture;
+}
+
+function heartOne() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("heart1").src = picture;
+}
+
+function heartTwo() {
+  let picture;
+  let boolean = true;
+  if (boolean = true) {
+    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+  } else {
+    boolean = false;
+  }
+  document.getElementById("heart2").src = picture;
+}
 function twoCardsFlipped() {
 
 }
