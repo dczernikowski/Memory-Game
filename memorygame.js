adfe3db7a8771ed3d3a043430af6a111e3715627
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2019-03-01 13:08:33 -0500

    changed to an array, clicking isn't changing the picture

--- memorygame.js
+++ memorygame.js
@@ -1,138 +1,84 @@
+var numClick = 0;
+let club1 = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png"
+let club2 = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png"
+let triangle1 = "https://www.emoji.co.uk/files/google-emojis/symbols-android/8122-down-pointing-small-red-triangle.png"
+let triangle2 = "https://www.emoji.co.uk/files/google-emojis/symbols-android/8122-down-pointing-small-red-triangle.png"
+let diamond1 = "http://www.jemome.com/cdn/2009/10/diamonds-card-game-icon_67838.png"
+let diamond2 = "http://www.jemome.com/cdn/2009/10/diamonds-card-game-icon_67838.png"
+let spade1 = "https://openclipart.org/image/2400px/svg_to_png/166674/outlinedspade.png"
+let spade2 = "https://openclipart.org/image/2400px/svg_to_png/166674/outlinedspade.png"
+let smiley1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png"
+let smiley2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png"
+let heart1 = "https://cdn.pixabay.com/photo/2012/05/07/18/37/heart-48942_960_720.png"
+let heart2 = "https://cdn.pixabay.com/photo/2012/05/07/18/37/heart-48942_960_720.png"
+var pic = [club1, club2, diamond1, diamond2, spade1, spade2, triangle1, triangle2, heart1, heart2, smiley1, smiley2];
+
 function restart() {
 
 }
 
 function clubOne() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("club1").src = picture;
+  numClick++
+  document.getElementById("club1").innerHTML = pic[0];
 }
 
 function clubTwo() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("club2").src = picture;
+  numClick++
+  document.getElementById("club2").innerHTML = pic[0];
 }
 
 function triangleOne() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("triangle1").src = picture;
+  numClick++
+  document.getElementById("triangle1").innerHTML = pic[6];
 }
 
 function triangleTwo() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("triangle2").src = picture;
+  numClick++
+  document.getElementById("triangle2").innerHTML = pic[7];
 }
 
 function diamondOne() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("diamond1").src = picture;
+  numClick++
+  document.getElementById("diamond1").innerHTML = pic[2];
 }
 
 function diamondTwo() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("diamond2").src = picture;
+  numClick++
+  document.getElementById("diamond2").innerHTML = pic[3];
 }
 
 function smileyOne() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("smiley1").src = picture;
+  numClick++
+  document.getElementById("smiley1").innerHTML = pic[10];
 }
 
 function smileyTwo() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("smiley2").src = picture;
+  numClick++
+  document.getElementById("smiley2").innerHTML = pic[11];
 }
 
 function spadeOne() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("spade1").src = picture;
+  numClick++
+  document.getElementById("spade1").innerHTML = pic[4];
 }
 
 function spadeTwo() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("spade2").src = picture;
+  numClick++
+  document.getElementById("spade1").innerHTML = pic[5];
 }
 
 function heartOne() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("heart1").src = picture;
+  numClick++
+  document.getElementById("heart1").innerHTML = pic[8];
 }
 
 function heartTwo() {
-  let picture;
-  let boolean = true;
-  if (boolean = true) {
-    picture = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
-  } else {
-    boolean = false;
-  }
-  document.getElementById("heart2").src = picture;
+  numClick++
+  document.getElementById("heart2").innerHTML = pic[9];
 }
-function twoCardsFlipped() {
 
+function twoCardsFlipped() {
+  if (numClick = 2) {
+
+  }
 }
