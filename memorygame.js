b4b75ce8a5a9a612a6d68b6642b6d68c541fd54e
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2019-03-25 13:23:44 -0400

    I wanna die

--- memorygame.js
+++ memorygame.js
@@ -11,25 +11,29 @@ let diamond = "http://www.jemome.com/cdn/2009/10/diamonds-card-game-icon_67838.p
 let spade = "https://openclipart.org/image/2400px/svg_to_png/166674/outlinedspade.png";
 let smiley = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png";
 let heart = "https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/Card_heart.svg/866px-Card_heart.svg.png";
-let zero = document.getElementById("0");
-let one = document.getElementById("1");
-let two = document.getElementById("2");
-let three = document.getElementById("3");
-let four = document.getElementById("4");
-let five = document.getElementById("5");
-let six = document.getElementById("6");
-let seven = document.getElementById("7");
-let eight = document.getElementById("8");
-let nine = document.getElementById("9");
-let ten = document.getElementById("10");
-let eleven = document.getElementById("11");
+let zero = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+let one = "http://www.jemome.com/cdn/2009/10/diamonds-card-game-icon_67838.png";
+let two = "https://openclipart.org/image/2400px/svg_to_png/166674/outlinedspade.png";
+let three = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png";
+let four = "https://www.emoji.co.uk/files/google-emojis/symbols-android/8122-down-pointing-small-red-triangle.png";
+let five = "https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/43-512.png";
+let six = "https://www.emoji.co.uk/files/google-emojis/symbols-android/8122-down-pointing-small-red-triangle.png";
+let seven = "https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/Card_heart.svg/866px-Card_heart.svg.png";
+let eight = "http://www.jemome.com/cdn/2009/10/diamonds-card-game-icon_67838.png";
+let nine = "https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/Card_heart.svg/866px-Card_heart.svg.png";
+let ten = "https://openclipart.org/image/2400px/svg_to_png/166674/outlinedspade.png";
+let eleven = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png";
 var pic = [club, diamond, spade, triangle, heart, smiley];
-var cards = [zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven];
+var cards = [
+  [zero, one, two, three],
+  [four, five, six, seven],
+  [eight, nine, ten, eleven],
+];
 var shuffleButton = document.getElementById("shuffle")
-
 function shuffle() {
-  pic.sort(function(a, b){return 0.5 - Math.random()});
-  cards.sort(function(a, b){return 0.5 - Math.random()});
+  cards[0].sort(function(a, b){return 0.5 - Math.random()});
+  cards[1].sort(function(a, b){return 0.5 - Math.random()});
+  cards[2].sort(function(a, b){return 0.5 - Math.random()});
   triangles = 0;
   clubs = 0;
   smileys = 0;
@@ -50,76 +54,76 @@ function shuffle() {
     document.getElementById('heart2').src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_grande.png?v=1535755695";
 }
 
-function clubOne() {
+function cellOne() {
   numClick++
   clubs++
-  document.getElementById("club1").src = pic[0];
+  document.getElementById("club1").src = cards[0][0];
 }
 
-function clubTwo() {
+function cellTwo() {
   numClick++
   clubs++
-  document.getElementById("club2").src = pic[0];
+  document.getElementById("club2").src = cards[1][1];
 }
 
-function triangleOne() {
+function cellThree() {
   numClick++
   triangles++
-  document.getElementById("triangle1").src = pic[3];
+  document.getElementById("triangle1").src = cards[1][0];
 }
 
-function triangleTwo() {
+function cellFour() {
   numClick++
   triangles++
-  document.getElementById("triangle2").src = pic[3];
+  document.getElementById("triangle2").src = cards[1][2];
 }
 
-function diamondOne() {
+function cellFive() {
   numClick++
   diamonds++
-  document.getElementById("diamond1").src = pic[1];
+  document.getElementById("diamond1").src = cards[0][1];
 }
 
-function diamondTwo() {
+function cellSix() {
   numClick++
   diamonds++
-  document.getElementById("diamond2").src = pic[1];
+  document.getElementById("diamond2").src = cards[2][0];
 }
 
-function smileyOne() {
+function cellSeven() {
   numClick++
   smileys++
-  document.getElementById("smiley1").src = pic[5];
+  document.getElementById("smiley1").src = cards[0][3];
 }
 
-function smileyTwo() {
+function cellEight() {
   numClick++
   smileys++
-  document.getElementById("smiley2").src = pic[5];
+  document.getElementById("smiley2").src = cards[2][3];
 }
 
-function spadeOne() {
+function cellNine() {
   numClick++
   spades++
-  document.getElementById("spade1").src = pic[2];
+  document.getElementById("spade1").src = cards[0][2];
 }
 
-function spadeTwo() {
+function cellTen() {
   numClick++
   spades++
-  document.getElementById("spade2").src = pic[2];
+  document.getElementById("spade2").src = cards[2][2];
 }
 
-function heartOne() {
+function cellEleven() {
   numClick++
   hearts++
-  document.getElementById("heart1").src = pic[4];
+  document.getElementById("heart1").src = cards[1][3];
 }
 
-function heartTwo() {
+function cellTwelve() {
   numClick++
   hearts++
-  document.getElementById("heart2").src = pic[4];
+  document.getElementById("heart2").src = cards[2][1];
 }
 
 function twoCardsFlipped() {
